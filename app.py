0f6770c2a8e3ec691348654b365c2a051778b99e
AhmedMohamed204 <122085868+AhmedMohamed204@users.noreply.github.com>
2024-02-16 15:52:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from telegram import Bot
 from telegram.ext import Updater
 from PresentationLayer import clsBot
-BOT_TOKEN:str = "TOKEN"
+BOT_TOKEN:str = "6499575874:AAF1JWagbmy7trIphPNf9I5tZMFfrz3APDE"
 def main():
     bot = clsBot(BOT_TOKEN)
     bot.Run()
